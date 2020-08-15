130d05e071fe379feb4b69197874e9170b770fd4
Jakub Panczyszyn <zatroxkp@gmail.com>
2020-08-15 13:56:48 +0100

    Add project files.

---
+++ FindLargestPrimeFactor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindLargestPrimeFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
